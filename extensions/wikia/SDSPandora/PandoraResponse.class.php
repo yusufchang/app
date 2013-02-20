11f460651ba90681b85f22091e0d493994b2f7d1
mech <mech@wikia-inc.com>
2013-02-20 14:58:35 +0100

    adding some comments to PandoraResponse

--- extensions/wikia/SDSPandora/PandoraResponse.class.php
+++ extensions/wikia/SDSPandora/PandoraResponse.class.php
@@ -1,8 +1,12 @@
 <?php
 
+/**
+ * Represents the result of calling SD REST service. It contains the status of the call and the response.
+ */
 class PandoraResponse {
 	/**
 	 * @param $status Status
+	 * @param $statusCode int
 	 * @param $response String
 	 */
 	public function __construct($status, $statusCode, $response) {
@@ -11,20 +15,33 @@ class PandoraResponse {
 		$this->response = $response;
 	}
 
+	/**
+	 * @return int
+	 */
 	public function getStatusCode() {
 		return $this->statusCode;
 	}
 
+	/**
+	 * @return bool
+	 */
 	public function isOK() {
 		return $this->status->isOK();
 	}
 
+	/**
+	 * @return String
+	 */
 	public function getMessage() {
 		return $this->status->getMessage();
 	}
 
 	private $jsonResponse = null;
 
+	/**
+	 * return JSON response decoded into PHP variable
+	 * @return mixed|null
+	 */
 	public function asJson() {
 		if ( is_null( $this->jsonResponse ) ) {
 			$this->jsonResponse = json_decode($this->response);
