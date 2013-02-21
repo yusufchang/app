6c915911575526d13e6c40806de5f7f788df2a0e
Rafal Leszczynski <rafal@wikia-inc.com>
2013-02-21 12:59:34 +0100

    list template fix to prevent all the imputs deletion

--- extensions/wikia/SDSVideoMetadata/templates/SDSVideoMetadata_reference_list.php
+++ extensions/wikia/SDSVideoMetadata/templates/SDSVideoMetadata_reference_list.php
@@ -7,13 +7,11 @@
 				<input type="hidden" name="<?= $id ?>[]" value="<?= $item['id'] ?>">
 				<button class="secondary remove"><?= wfMsg('sdsvideometadata-vc-remove-item')?></button>
 			</li>
-			<?php endforeach; ?>
-		<?php if (count($list) === 0) : ?>
-			<li>
-				<input type="text" name="<?= $name ?>[]" id="<?= $name ?>">
-				<button class="secondary remove hidden"><?= wfMsg('sdsvideometadata-vc-remove-item')?></button>
-			</li>
-		<?php endif; ?>
+		<?php endforeach; ?>
+		<li>
+			<input type="text" name="<?= $name ?>[]" id="<?= $name ?>">
+			<button class="secondary remove hidden"><?= wfMsg('sdsvideometadata-vc-remove-item')?></button>
+		</li>
 	</ul>
 	<button class="add secondary"><?= wfMsg('sdsvideometadata-vc-add-item')?></button>
 </div>
