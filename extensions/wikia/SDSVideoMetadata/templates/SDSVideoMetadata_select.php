9638859954b8de6e4697699bb28322473c4111aa
Adam Robak <adamr@wikia-inc.com>
2013-02-22 15:31:36 +0100

    added empty value to select type

--- extensions/wikia/SDSVideoMetadata/templates/SDSVideoMetadata_select.php
+++ extensions/wikia/SDSVideoMetadata/templates/SDSVideoMetadata_select.php
@@ -1,6 +1,7 @@
 <div class="input-group <?= $type ?>">
 	<label for="<?= $name ?>"><?= $labelMsg ?></label>
 	<select name="<?= $name ?>" id="<?= $name ?>">
+		<option value="" >...</option>
 		<?php foreach ($options as $option): ?>
 			<option value="<?= htmlspecialchars($option['value']) ?>" <?= (!empty($selected) && ( $selected === $option['value'] ) ) ?
 				'selected="selected"' : '' ?>><?= htmlspecialchars($option['text']) ?></option>
