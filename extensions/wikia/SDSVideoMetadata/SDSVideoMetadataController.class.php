d4516a0d649ed78e524450ed6c1eaf53feb2ae5e
Rafal Leszczynski <rafal@wikia-inc.com>
2013-03-13 18:22:35 +0100

    rename CSS file

--- extensions/wikia/SDSVideoMetadata/SDSVideoMetadataController.class.php
+++ extensions/wikia/SDSVideoMetadata/SDSVideoMetadataController.class.php
@@ -15,7 +15,7 @@ class SDSVideoMetadataController extends WikiaSpecialPageController {
 
 	public function index() {
 
-		$this->response->addAsset('extensions/wikia/SDSVideoMetadata/css/SDSVideoMetadata.scss');
+		$this->response->addAsset('extensions/wikia/SDSVideoMetadata/css/VideoMetadata.scss');
 		$this->response->addAsset('extensions/wikia/SDSVideoMetadata/js/VideoMetadata.js');
 		$file = $this->getVal('video');
 
