66d70545982f947e7cd916f5afb32fcbc179509f
Adam Robak <adamr@wikia-inc.com>
2013-03-28 16:48:47 +0100

    PLA-99 tests for parsing request

--- extensions/wikia/SDSPandora/tests/PandoraFormsTest.php
+++ extensions/wikia/SDSPandora/tests/PandoraFormsTest.php
@@ -27,11 +27,19 @@ class PandoraFormsTest extends WikiaBaseTest {
 	public function prepareOrm( $data ) {
 		$this->setUp();
 		//should return PandoraORM typed object
+		PandoraORM::$config = array(
+			'id' => array( 'type' => PandoraSDSObject::TYPE_LITERAL, 'subject' => 'id' ),
+			'name' => array( 'type'=>PandoraSDSObject::TYPE_LITERAL, 'subject'=>'schema:name' ),
+			'description' => array( 'type'=>PandoraSDSObject::TYPE_LITERAL, 'subject'=>'schema:description' ),
+			'type' => array( 'type'=>PandoraSDSObject::TYPE_LITERAL, 'subject'=>'type' ),
+			'about' => array( 'type'=>PandoraSDSObject::TYPE_COLLECTION, 'subject'=>'schema:about', 'childType'=>'schema:Movie'),
+			'about_person' => array( 'type'=>PandoraSDSObject::TYPE_COLLECTION, 'subject'=>'schema:about', 'childType' => 'schema:Person' ),
+			'about_literal' => array( 'type'=>PandoraSDSObject::TYPE_COLLECTION, 'subject'=>'schema:about' )
+		);
 		$orm = PandoraORM::buildFromType( 'TestType' );
 		foreach ( $data as $key => $value ) {
 			$orm->set( $key, $value );
 		}
-		var_dump( $orm );
 		return $orm;
 	}
 
@@ -56,6 +64,20 @@ class PandoraFormsTest extends WikiaBaseTest {
 	 */
 	public function testLoadFromORM( $orm, $config ) {
 		$formsHelper = $this->preparePandoraForms( $config );
+		$formsHelper->loadFromORM( $orm );
+
+		print_r( $formsHelper->getData() );
+	}
+
+	/**
+	 * @dataProvider requestDataProvider
+	 * @param $data
+	 * @param $config
+	 */
+	public function testGetOrmFromRequest( $data, $config ) {
+		$formsHelper = $this->preparePandoraForms( $config );
+		$orm = $formsHelper->getOrmFromRequest( $data, 'http://sds.wikia.com/sds/testObject' );
+		print_r( $orm );
 	}
 
 
@@ -72,13 +94,13 @@ class PandoraFormsTest extends WikiaBaseTest {
 		return array(
 			array(
 				array(
-				'videoObject_description' => array(
-					'controller' => 'PandoraForms',
-					'template' => 'default',
-					'name' => 'videoObject_description',
-					'textarea' => true,
-					'label' => 'sdsvideometadata-vc-description'
-				)
+					'videoObject_description' => array(
+						'controller' => 'PandoraForms',
+						'template' => 'default',
+						'name' => 'videoObject_description',
+						'textarea' => true,
+						'label' => 'sdsvideometadata-vc-description'
+					)
 				),
 				array(
 					'videoObject_description' => 'mocked description'
@@ -145,12 +167,75 @@ class PandoraFormsTest extends WikiaBaseTest {
 				$this->prepareOrm( array(
 					'name' => 'testName',
 					'type' => 'schema:VideoObject',
-					'description' => 'simple description'
+					'description' => 'simple description',
+					'about' => array(
+						'id' => 'http://sds.wikia.com/sds/about',
+						'name' => 'subitem movie',
+						'description' => 'subitem movie description',
+						'type' => 'schema:Movie'
+					),
+					'about_person' => array(
+						'id' => 'http://sds.wikia.com/sds/about_person',
+						'name' => 'subitem person',
+						'description' => 'subitem person description',
+						'type' => 'schema:Person'
+					),
+					'about_literal' => array(
+						'literal1',
+						'literal2'
+					)
 				) ),
-				array(
-
-				)
 				//config
+				array(
+					'videoObject_description' => array(
+						'controller' => 'PandoraForms',
+						'template' => 'default',
+						'textarea' => true,
+						'label' => 'sdsvideometadata-vc-description',
+						'ormKey' => 'description'
+					),
+					'about_movie' => array(
+						'controller' => 'PandoraForms',
+						'template' => 'default',
+						'textarea' => true,
+						'label' => 'sdsvideometadata-vc-description',
+						'ormKey' => 'about_literal',
+//						'childKey' => 'description'
+					)
+				)
+			)
+		);
+	}
+
+	public function requestDataProvider() {
+		return array(
+			array(
+				//data
+				array(
+					'videoObject_description' => 'some description',
+					'about_movie' => array(
+						array( 'name' => 'first element', 'id' => 'http://sds.wikia.com/sds/existingOne'),
+						array( 'name' => 'second element', 'id' => '')
+					)
+				),
+				//config
+				array(
+					'videoObject_description' => array(
+						'controller' => 'PandoraForms',
+						'template' => 'default',
+						'textarea' => true,
+						'label' => 'sdsvideometadata-vc-description',
+						'ormKey' => 'description'
+					),
+					'about_movie' => array(
+						'controller' => 'PandoraForms',
+						'template' => 'default',
+						'textarea' => true,
+						'label' => 'sdsvideometadata-vc-description',
+						'ormKey' => 'about',
+//						'childKey' => 'description'
+					)
+				)
 			)
 		);
 	}
