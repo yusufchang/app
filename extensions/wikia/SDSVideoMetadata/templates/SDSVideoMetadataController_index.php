6b24d64a9aa6e3bcb7a88cb1260ff59cbc25b91f
Adam Robak <adamr@wikia-inc.com>
2013-03-29 16:51:41 +0100

    PLA-99 changed controller to work with PandorForms

--- extensions/wikia/SDSVideoMetadata/templates/SDSVideoMetadataController_index.php
+++ extensions/wikia/SDSVideoMetadata/templates/SDSVideoMetadataController_index.php
@@ -15,26 +15,32 @@
 	//			)); ?>
 
 				<!-- Description -->
-				<?= F::app()->renderPartial('SDSVideoMetadataController', 'default', array(
-				'name' => 'videoObject_description',
-				'textarea' => true,
-				'labelMsg' => wfMessage('sdsvideometadata-vc-description')->text(),
-				'value' => isset( $vcObj['videoObject_description'] ) ? $vcObj['videoObject_description'] : null
-				)); ?>
+				<?= $formBuilder->renderField( 'videoObject_description' ); ?>
+
+<!--				--><?//= F::app()->renderPartial('SDSVideoMetadataController', 'default', array(
+//				'name' => 'videoObject_description',
+//				'textarea' => true,
+//				'labelMsg' => wfMessage('sdsvideometadata-vc-description')->text(),
+//				'value' => isset( $vcObj['videoObject_description'] ) ? $vcObj['videoObject_description'] : null
+//				)); ?>
 
 				<!-- Language -->
-				<?= F::app()->renderPartial('SDSVideoMetadataController', 'default', array(
-				'name' => 'videoObject_inLanguage',
-				'labelMsg' => wfMessage('sdsvideometadata-vc-language')->text(),
-				'value' => isset( $vcObj['videoObject_inLanguage'] ) ? $vcObj['videoObject_inLanguage'] : null
-				)); ?>
+				<?= $formBuilder->renderField( 'videoObject_inLanguage' ); ?>
+
+<!--				--><?//= F::app()->renderPartial('SDSVideoMetadataController', 'default', array(
+//				'name' => 'videoObject_inLanguage',
+//				'labelMsg' => wfMessage('sdsvideometadata-vc-language')->text(),
+//				'value' => isset( $vcObj['videoObject_inLanguage'] ) ? $vcObj['videoObject_inLanguage'] : null
+//				)); ?>
 
 				<!-- Subtitles -->
-				<?= F::app()->renderPartial('SDSVideoMetadataController', 'default', array(
-				'name' => 'videoObject_subTitleLanguage',
-				'labelMsg' => wfMessage('sdsvideometadata-vc-subtitles')->text(),
-				'value' => isset( $vcObj['videoObject_subTitleLanguage'] ) ? $vcObj['videoObject_subTitleLanguage'] : null
-				)); ?>
+				<?= $formBuilder->renderField( 'videoObject_subTitleLanguage' ); ?>
+
+<!--				--><?//= F::app()->renderPartial('SDSVideoMetadataController', 'default', array(
+//				'name' => 'videoObject_subTitleLanguage',
+//				'labelMsg' => wfMessage('sdsvideometadata-vc-subtitles')->text(),
+//				'value' => isset( $vcObj['videoObject_subTitleLanguage'] ) ? $vcObj['videoObject_subTitleLanguage'] : null
+//				)); ?>
 
 				<!-- Video object type selection -->
 				<div class="input-group">
@@ -61,188 +67,72 @@
 				<legend class="VMD-header"><?= wfMessage('sdsvideometadata-type-specific-metadata-legend')->text() ?></legend>
 
 				<!-- Recipe -->
-				<?= F::app()->renderPartial('SDSVideoMetadataController', 'reference_list', array(
-				'type' => 'VideoClipCookingVideo',
-				'name' => 'recipe_name',
-				'id' => 'recipe_id',
-				'labelMsg' => wfMessage('sdsvideometadata-vc-recipe')->text(),
-				'list' => isset( $vcObj['recipe_name'] ) ? $vcObj['recipe_name'] : null
-				)); ?>
+				<?= $formBuilder->renderField( 'recipe_name' ); ?>
+<!--				--><?//= F::app()->renderPartial('SDSVideoMetadataController', 'reference_list', array(
+//				'type' => 'VideoClipCookingVideo',
+//				'name' => 'recipe_name',
+//				'id' => 'recipe_id',
+//				'labelMsg' => wfMessage('sdsvideometadata-vc-recipe')->text(),
+//				'list' => isset( $vcObj['recipe_name'] ) ? $vcObj['recipe_name'] : null
+//				)); ?>
 
 				<!-- Distributor -->
-				<?= F::app()->renderPartial('SDSVideoMetadataController', 'reference_list', array(
-				'type' => 'VideoClipTravelVideo VideoClipCookingVideo VideoClipCraftVideo VideoClipHowToVideo',
-				'name' => 'provider_name',
-				'id' => 'provider_id',
-				'labelMsg' => wfMessage('sdsvideometadata-vc-distributor')->text(),
-				'list' => isset( $vcObj['provider_name'] ) ? $vcObj['provider_name'] : null
-				)); ?>
+				<?= $formBuilder->renderField( 'provider_name' ); ?>
+<!--				--><?//= F::app()->renderPartial('SDSVideoMetadataController', 'reference_list', array(
+//				'type' => 'VideoClipTravelVideo VideoClipCookingVideo VideoClipCraftVideo VideoClipHowToVideo',
+//				'name' => 'provider_name',
+//				'id' => 'provider_id',
+//				'labelMsg' => wfMessage('sdsvideometadata-vc-distributor')->text(),
+//				'list' => isset( $vcObj['provider_name'] ) ? $vcObj['provider_name'] : null
+//				)); ?>
 
 				<!-- Publisher -->
-				<?= F::app()->renderPartial('SDSVideoMetadataController', 'reference_list', array(
-				'type' => 'VideoClipTravelVideo VideoClipCookingVideo VideoClipCraftVideo VideoClipHowToVideo',
-				'name' => 'publisher_name',
-				'id' => 'publisher_id',
-				'labelMsg' => wfMessage('sdsvideometadata-vc-publisher')->text(),
-				'list' => isset( $vcObj['publisher_name'] ) ? $vcObj['publisher_name'] : null
-				)); ?>
+				<?= $formBuilder->renderField( 'publisher_name' ); ?>
 
 				<!-- Song -->
-				<?= F::app()->renderPartial('SDSVideoMetadataController', 'reference_list', array(
-				'type' => 'VideoClipMusicVideo',
-				'name' => 'track_name',
-				'id' => 'track_id',
-				'labelMsg' => wfMessage('sdsvideometadata-vc-song')->text(),
-				'list' => isset( $vcObj['track_name'] ) ? $vcObj['track_name'] : null
-				)); ?>
+				<?= $formBuilder->renderField( 'track_name' ); ?>
 
 				<!-- Artist -->
-				<?= F::app()->renderPartial('SDSVideoMetadataController', 'reference_list', array(
-				'type' => 'VideoClipMusicVideo',
-				'name' => 'musicGroup_name',
-				'id' => 'musicGroup_id',
-				'labelMsg' => wfMessage('sdsvideometadata-vc-artist')->text(),
-				'list' => isset( $vcObj['musicGroup_name'] ) ? $vcObj['musicGroup_name'] : null
-				)); ?>
+				<?= $formBuilder->renderField( 'musicGroup_name' ); ?>
 
 				<!-- Music Label -->
-				<?= F::app()->renderPartial('SDSVideoMetadataController', 'reference_list', array(
-				'type' => 'VideoClipMusicVideo',
-				'name' => 'musicRecording_musicLabel',
-				'id' => 'musicRecording_musicLabel_id',
-				'labelMsg' => wfMessage('sdsvideometadata-vc-music-label')->text(),
-				'list' => isset( $vcObj['musicRecording_musicLabel'] ) ? $vcObj['musicRecording_musicLabel'] : null
-				)); ?>
+				<?= $formBuilder->renderField( 'musicRecording_musicLabel' ); ?>
 
 				<!-- Genre -->
-				<?= F::app()->renderPartial('SDSVideoMetadataController', 'literal_list', array(
-				'type' => 'VideoClipTravelVideo VideoClipMusicVideo VideoClipCookingVideo
-						VideoClipCraftVideo VideoClipHowToVideo',
-				'name' => 'videoObject_genre',
-				'id' => 'videoObject_genre_id',
-				'labelMsg' => wfMessage('sdsvideometadata-vc-genre')->text(),
-				'list' => isset( $vcObj['videoObject_genre'] ) ? $vcObj['videoObject_genre'] : null
-				)); ?>
+				<?= $formBuilder->renderField( 'videoObject_genre' ); ?>
 
 				<!-- Location -->
-				<?= F::app()->renderPartial('SDSVideoMetadataController', 'reference_list', array(
-				'type' => 'VideoClipTravelVideo',
-				'name' => 'about_location',
-				'id' => 'about_location_id',
-				'labelMsg' => wfMessage('sdsvideometadata-vc-location')->text(),
-				'list' => isset( $vcObj['about_location'] ) ? $vcObj['about_location'] : null
-				)); ?>
+				<?= $formBuilder->renderField( 'about_location' ); ?>
 
 				<!-- Game -->
-				<?= F::app()->renderPartial('SDSVideoMetadataController', 'reference_list', array(
-				'type' => 'VideoClipGamingVideo',
-				'name' => 'about_name',
-				'id' => 'about_id',
-				'labelMsg' => wfMessage('sdsvideometadata-vc-game')->text(),
-				'list' => isset( $vcObj['about_name'] ) ? $vcObj['about_name'] : null
-				)); ?>
+				<?= $formBuilder->renderField( 'about_name' ); ?>
 
 				<!-- TV Series -->
-				<?= F::app()->renderPartial('SDSVideoMetadataController', 'reference_list', array(
-				'type' => 'VideoClipTVVideo',
-				'name' => 'series_name',
-				'id' => 'series_id',
-				'labelMsg' => wfMessage('sdsvideometadata-vc-series')->text(),
-				'list' => isset( $vcObj['series_name'] ) ? $vcObj['series_name'] : null
-				)); ?>
+				<?= $formBuilder->renderField( 'series_name' ); ?>
 
 				<!-- Season -->
-				<?= F::app()->renderPartial('SDSVideoMetadataController', 'reference_list', array(
-				'type' => 'VideoClipTVVideo',
-				'name' => 'season_name',
-				'id' => 'season_id',
-				'labelMsg' => wfMessage('sdsvideometadata-vc-season')->text(),
-				'list' => isset( $vcObj['season_name'] ) ? $vcObj['season_name'] : null
-				)); ?>
+				<?= $formBuilder->renderField( 'season_name' ); ?>
 
 				<!-- Movie -->
-				<?= F::app()->renderPartial('SDSVideoMetadataController', 'reference_list', array(
-				'type' => 'VideoClipMovieTrailersVideo',
-				'name' => 'movie_name',
-				'id' => 'movie_id',
-				'labelMsg' => wfMessage('sdsvideometadata-vc-movie')->text(),
-				'list' => isset( $vcObj['movie_name'] ) ? $vcObj['movie_name'] : null
-				)); ?>
+				<?= $formBuilder->renderField( 'movie_name' ); ?>
 
 				<!-- Trailer rating  -->
-				<?= F::app()->renderPartial('SDSVideoMetadataController', 'default', array(
-				'type' => 'VideoClipMovieTrailersVideo',
-				'name' => 'videoObject_rating',
-				'labelMsg' => wfMessage('sdsvideometadata-vc-trailer-rating')->text(),
-				'value' => isset( $vcObj['videoObject_rating'] ) ? $vcObj['videoObject_rating'] : null
-				)); ?>
+				<?= $formBuilder->renderField( 'videoObject_rating' ); ?>
 
 				<!-- Type -->
-				<?= F::app()->renderPartial('SDSVideoMetadataController', 'literal_list', array(
-				'type' => 'VideoClipGamingVideo VideoClipTVVideo',
-				'name' => 'videoObject_keywords',
-				'id' => 'videoObject_keywords_id',
-				'labelMsg' => wfMessage('sdsvideometadata-vc-kind')->text(),
-				'list' => isset( $vcObj['videoObject_keywords'] ) ? $vcObj['videoObject_keywords'] : null
-				)); ?>
+				<?= $formBuilder->renderField( 'videoObject_keywords' ); ?>
 
 				<!-- Age gate -->
-				<?= F::app()->renderPartial('SDSVideoMetadataController', 'select', array(
-				'type' => 'VideoClipGamingVideo VideoClipMovieTrailersVideo',
-				'name' => 'videoObject_isFamilyFriendly',
-				'labelMsg' => wfMessage('sdsvideometadata-vc-age-gate')->text(),
-				'options' => array(
-					array(
-						'value' => '',
-						'text' => wfMessage('sdsvideometadata-vc-boolean-not-set')->text()
-					),
-					array(
-						'value' => 'true',
-						'text' => wfMessage('sdsvideometadata-vc-boolean-true')->text()
-					),
-					array(
-						'value' => 'false',
-						'text' => wfMessage('sdsvideometadata-vc-boolean-false')->text()
-					)
-				),
-				'selected' => isset( $vcObj['videoObject_isFamilyFriendly'] ) ? $vcObj['videoObject_isFamilyFriendly'] : null
-				)); ?>
+				<?= $formBuilder->renderField( 'videoObject_isFamilyFriendly' ); ?>
 
 				<!-- PAL -->
-				<?= F::app()->renderPartial('SDSVideoMetadataController', 'select', array(
-				'type' => 'VideoClipMusicVideo',
-				'name' => 'videoObject_contentFormat',
-				'labelMsg' => wfMessage('sdsvideometadata-vc-pal')->text(),
-				'options' => array(
-					array(
-						'value' => '',
-						'text' => wfMessage('sdsvideometadata-vc-boolean-not-set')->text()
-					),
-					array(
-						'value' => 'PAL',
-						'text' => wfMessage('sdsvideometadata-vc-boolean-true')->text()
-					)
-				),
-				'selected' => isset( $vcObj['videoObject_contentFormat'] ) ? $vcObj['videoObject_contentFormat'] : null
-				)); ?>
+				<?= $formBuilder->renderField( 'videoObject_contentFormat' ); ?>
 
 				<!-- Soundtrack -->
-				<?= F::app()->renderPartial('SDSVideoMetadataController', 'reference_list', array(
-				'type' => 'VideoClipGamingVideo',
-				'name' => 'videoObject_associatedMedia',
-				'id' => 'videoObject_associatedMedia_id',
-				'labelMsg' => wfMessage('sdsvideometadata-vc-soundtrack')->text(),
-				'list' => isset( $vcObj['videoObject_associatedMedia'] ) ? $vcObj['videoObject_associatedMedia'] : null
-				)); ?>
+				<?= $formBuilder->renderField( 'videoObject_associatedMedia' ); ?>
 
 				<!-- Setting -->
-				<?= F::app()->renderPartial('SDSVideoMetadataController', 'literal_list', array(
-				'type' => 'VideoClipGamingVideo VideoClipMusicVideo VideoClipTVVideo VideoClipMovieTrailersVideo',
-				'name' => 'videoObject_setting',
-				'id' => 'videoObject_setting_id',
-				'labelMsg' => wfMessage('sdsvideometadata-vc-setting')->text(),
-				'list' => isset( $vcObj['videoObject_setting'] ) ? $vcObj['videoObject_setting'] : null
-				)); ?>
+				<?= $formBuilder->renderField( 'videoObject_setting' ); ?>
 
 			</fieldset>
 
