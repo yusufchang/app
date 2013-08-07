75f745bad495505e919d153671ebc5fd5511ac7a
Adam Robak <adamr@wikia-inc.com>
2013-08-07 14:59:13 +0200

    maintenance script for invalidating top module

---
+++ maintenance/wikia/topmodule_invalidate.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by adam
+ * Date: 07.08.13
+ */
+
+require_once( dirname(__FILE__) . '/../Maintenance.php' );
+
+class TopModuleInvalidate extends Maintenance {
+
+	public function execute() {
+		global $wgMemc;
+		if ( !empty( $_ENV['SERVER_ID'] ) && $wgMemc ) {
+			$cacheKey = wfMemcKey( __CLASS__, 'WikiaSearch', 'topWikiArticles', $_ENV['SERVER_ID'] );
+			$wgMemc->set( $cacheKey, null, 1 );
+		}
+	}
+}
+
+$maintClass = "TopModuleInvalidate";
+require_once( DO_MAINTENANCE );
