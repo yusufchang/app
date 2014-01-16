447ca325ca6e99884953e79078ff518a3e9c770a
Adam Robak <adamr@wikia-inc.com>
2014-01-16 09:52:42 +0100

    Infobox

--- extensions/wikia/SpecialInfoboxMapper/SpecialInfoboxMapperController.class.php
+++ extensions/wikia/SpecialInfoboxMapper/SpecialInfoboxMapperController.class.php
@@ -15,25 +15,35 @@ class SpecialInfoboxMapperController extends WikiaSpecialPageController {
 	public function index() {
 		$k = isset( $_REQUEST['k'] ) ? $_REQUEST['k'] : 0 ;
 
-		if ( isset( $_POST['type'] ) ) {
+		if ( !empty( $_POST ) ) {
 			$this->saveType( $_POST );
 		}
 
 		//get next key and data
-		$current = $this->getList($k);
+		$current = $this->getBatch($k);
 
 		$this->setVal( 'data', $current );
-		$this->setVal( 'k', ++$k );
+		$this->setVal( 'k', $k );
 	}
 
 	protected function saveType( $values ) {
-		var_dump( 'saved' );
-		var_dump( $values );
+		$db = wfGetDB(DB_MASTER, array(), F::app()->wg->ExternalDatawareDB);
+		$num = ( count( $values ) - 1 ) / 3;
+		//prepare data
+		for ( $i = 0; $i < $num; $i++ ) {
+			if ( !empty( $values[ 'type_'.$i ] ) ) {
+				(new WikiaSQL())->UPDATE( 'info_schema_map' )
+					->SET( 'type', $values[ 'type_'.$i ] )
+					->WHERE( 'info_key' )->EQUAL_TO( $values[ 'key_'.$i ] )
+					->AND_( 'template' )->EQUAL_TO( $values[ 'template_'.$i ] )
+				->run( $db, function(){});
+			}
+		}
 	}
 
-	protected function getList( $k ) {
-		$list = $this->service->getKeys();
-		$batches = array_chunk( $list, 10 );
+	protected function getBatch( $k ) {
+		$all = $this->service->getSchemaKeys();
+		$batches = array_chunk( $all, 25 );
 		return $batches[$k];
 	}
 
--- extensions/wikia/SpecialInfoboxMapper/templates/SpecialInfoboxMapper_index.php
+++ extensions/wikia/SpecialInfoboxMapper/templates/SpecialInfoboxMapper_index.php
@@ -1,16 +1,26 @@
 <form method="post">
 
-<? foreach( $data as $i => $d ) : ?>
-	<h3><?= $d['key'] ?></h3>
-	<h3><?= $d['template'] ?></h3>
-	<div>
-<!--		--><?//= $d['samples'] ?>
-	</div>
-	<label for="type_id">Schema type</label>
-	<input id="type_id" type="text" name="type_<?= $i ?>" />
-	<input type="hidden" name="key_<?= $i ?>" value="<?= $d['key'] ?>"/>
-	<input type="hidden" name="k" value="<?= $k ?>"/>
-<? endforeach ?>
+	<table>
+		<? foreach( $data as $i => $d ) : ?>
+			<tr>
+				<td><?= $d['info_key'] ?></td>
+				<td><?= $d['template'] ?></td>
+				<div>
+					<!--		--><?//= $d['samples'] ?>
+				</div>
+				<td>
+					<label for="type_id">Schema type</label>
+					<input id="type_id" type="text" name="type_<?= $i ?>" value="<?= $d['type'] ?>" />
+				</td>
+				<input type="hidden" name="key_<?= $i ?>" value="<?= $d['info_key'] ?>"/>
+				<input type="hidden" name="template_<?= $i ?>" value="<?= $d['template'] ?>"/>
+				<input type="hidden" name="k" value="<?= $k ?>"/>
+			</tr>
+		<? endforeach ?>
+	</table>
 
 	<button type="submit">Save</button>
 </form>
+
+<a href="/Special:InfoboxMapper?k=<?= $k-1 ?>">Prev</a>
+<a href="/Special:InfoboxMapper?k=<?= $k+1 ?>">Next</a>
