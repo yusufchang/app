83077aab0dcde849d9d47acc76aa25b624238de7
Adam Robak <adamr@wikia-inc.com>
2014-03-17 13:56:19 +0100

    PLA-1272 exact match to tv match changed

--- maintenance/wikia/SolrIndexer/TvSolrIndexer.php
+++ maintenance/wikia/SolrIndexer/TvSolrIndexer.php
@@ -41,7 +41,7 @@ class TvSolrIndexer extends Maintenance {
 
 		$this->data[ $row[ 'wiki_id' ] ][ $row[ 'wiki_lang' ] ][] = $row['series_name'];
 		$this->data[ $row[ 'wiki_id' ] ][ 'txt' ][] = $row['series_name'];
-		$this->data[ $row[ 'wiki_id' ] ][ 'em' ][] = $row['series_name'];
+		$this->data[ $row[ 'wiki_id' ] ][ 'tm' ][] = $row['series_name'];
 	}
 
 	protected function createSolrUpdate() {
