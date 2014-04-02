7d2e61cfeb267853794f9268740ef7b3380b63ee
Piotr Gabryjeluk <rychu@wikia-inc.com>
2014-04-02 14:43:58 +0200

    ADEN-974 Move INVISIBLE_SKIN above TOP_LEADERBOARD

--- skins/oasis/modules/templates/Ad_Top.php
+++ skins/oasis/modules/templates/Ad_Top.php
@@ -1,28 +1,26 @@
 <div class="WikiaTopAds" id="WikiaTopAds">
-<div class="WikiaTopAdsInner">
+	<?= $app->renderView('Ad', 'Index', array('slotname' => 'INVISIBLE_SKIN')); ?>
+	<div class="WikiaTopAdsInner">
 
 <?php
 
-if (WikiaPageType::isWikiaHub()) {
-	echo $app->renderView('Ad', 'Index', array('slotname' => 'HUB_TOP_LEADERBOARD'));
-} elseif ($wg->EnableWikiaHomePageExt) {
-	if (WikiaPageType::isSearch()) {
-		echo $app->renderView('Ad', 'Index', array('slotname' => 'TOP_LEADERBOARD'));
+	if (WikiaPageType::isWikiaHub()) {
+		echo $app->renderView('Ad', 'Index', array('slotname' => 'HUB_TOP_LEADERBOARD'));
+	} elseif ($wg->EnableWikiaHomePageExt) {
+		if (WikiaPageType::isSearch()) {
+			echo $app->renderView('Ad', 'Index', array('slotname' => 'TOP_LEADERBOARD'));
+		} else {
+			echo $app->renderView('Ad', 'Index', array('slotname' => 'CORP_TOP_LEADERBOARD'));
+		}
+	} elseif (WikiaPageType::isMainPage()) {
+		echo $app->renderView('Ad', 'Index', array('slotname' => 'HOME_TOP_LEADERBOARD'));
 	} else {
-		echo $app->renderView('Ad', 'Index', array('slotname' => 'CORP_TOP_LEADERBOARD'));
+		echo $app->renderView('Ad', 'Index', array('slotname' => 'TOP_LEADERBOARD'));
 	}
-} elseif (WikiaPageType::isMainPage()) {
-	echo $app->renderView('Ad', 'Index', array('slotname' => 'HOME_TOP_LEADERBOARD'));
-} else {
-	echo $app->renderView('Ad', 'Index', array('slotname' => 'TOP_LEADERBOARD'));
-}
 
-echo $app->renderView('Ad', 'Index', array('slotname' => 'TOP_BUTTON_WIDE'));
+	echo $app->renderView('Ad', 'Index', array('slotname' => 'TOP_BUTTON_WIDE'));
 
 ?>
 
-</div>
-
-<?= $app->renderView('Ad', 'Index', array('slotname' => 'INVISIBLE_SKIN')); ?>
-
+	</div>
 </div>
