2a3aeae5537ac6315238915c8c3917fcbdd771be
Krzysztof 'suchy' Suchanek <suchy@wikia-inc.com>
2014-05-28 16:28:57 +0200

    thumbnail fix

--- extensions/wikia/HubRssFeed/BaseRssModel.class.php
+++ extensions/wikia/HubRssFeed/BaseRssModel.class.php
@@ -206,16 +206,19 @@ abstract class BaseRssModel extends WikiaService {
 
 	protected function getArticleDetail( $wikiId, $articleId ) {
 		$host = WikiFactory::DBtoUrl( WikiFactory::IDtoDB( $wikiId ) );
-		$url = sprintf( '%sapi/v1/Articles/Details?crop=false&ids=%u', $host, $articleId );
+		$url = sprintf( '%sapi/v1/Articles/Details?ids=%u', $host, $articleId );
 		$res = Http::get( $url );
 		$res = json_decode( $res, true );
 		$article = $res[ 'items' ][ $articleId ];
 		if ( !$article[ 'thumbnail' ] ) {
 			$ws = new WikiService();
 			$article[ 'thumbnail' ] = $ws->getWikiWordmark( $wikiId );
+		}else{
+			$article[ 'thumbnail' ] = preg_replace( '/(.*\/)(.*)\/.*/', '$1$2/-$2', $article[ 'thumbnail' ] );
 		}
+
 		return [ 'img' => [
-			'url' => $article[ 'thumbnail' ],
+			'url' =>  $article[ 'thumbnail' ], //remove cropping info from image url
 			'width' => $article[ 'original_dimensions' ][ 'width' ] < self::MIN_IMAGE_SIZE ? self::MIN_IMAGE_SIZE : $article[ 'original_dimensions' ][ 'width' ],
 			'height' => $article[ 'original_dimensions' ][ 'height' ] < self::MIN_IMAGE_SIZE ? self::MIN_IMAGE_SIZE : $article[ 'original_dimensions' ][ 'width' ]
 		],
