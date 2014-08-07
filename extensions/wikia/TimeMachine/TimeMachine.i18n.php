6682761b07110ac11c4252c4209ccb3971e83c8b
Garth Webb <garth@wikia-inc.com>
2014-08-07 11:18:04 -0700

    Missed a file

---
+++ extensions/wikia/TimeMachine/TimeMachine.i18n.php
@@ -0,0 +1,11 @@
+<?php
+
+$messages = array();
+
+$messages['en'] = array(
+	'timemachine-desc' => 'Extension to include the TimeMachine module on a wikia',
+);
+
+$messages['qqq'] = array(
+	'timemachine-desc' => 'Description for the TimeMachine extension',
+);
