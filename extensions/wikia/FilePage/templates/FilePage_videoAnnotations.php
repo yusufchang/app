ce5327caea028e12a175847a15bb5e2bb7c72236
lizlux <liz@lizlux.net>
2014-08-08 14:15:36 -0700

    fixing message

--- extensions/wikia/FilePage/templates/FilePage_videoAnnotations.php
+++ extensions/wikia/FilePage/templates/FilePage_videoAnnotations.php
@@ -1,4 +1,4 @@
 <div data-tab-body="annotations" class="tabBody">
-	<h3>Click on the video as it's playing to add annotations. A form field will display below where you can edit annotation info.</h3>
+	<p>Click on the video as it's playing to add annotations. A form field will display below where you can edit annotation info.</p>
 	<div class="video-annotations-wrapper WikiaForm"><?= $annotations ?></div>
 </div>
