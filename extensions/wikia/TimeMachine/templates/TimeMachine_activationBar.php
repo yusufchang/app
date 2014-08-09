a050bf384ce6d225ad0d4f897140ecc7b0d959ca
Christian Williams <christian@wikia-inc.com>
2014-08-09 00:55:32 +0000

    more styling on activation bar

--- extensions/wikia/TimeMachine/templates/TimeMachine_activationBar.php
+++ extensions/wikia/TimeMachine/templates/TimeMachine_activationBar.php
@@ -1,7 +1,7 @@
 <div class="inner clearfix">
 	<h1>Not all caught up?</h1>
 	<div class="settings">
-		<p>Activate SpoilerGuard</p>
+		<p>Activate<br>Spoiler Guard</p>
 		<select class="WikiaSeason"></select>
 		<select class="WikiaEpisode"></select>
 	</div>
