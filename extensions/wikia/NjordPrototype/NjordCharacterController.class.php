bfc9f0897e5ac91a62717cc5f3e9b191d24540cb
Adam Robak <adamr@wikia-inc.com>
2014-11-11 15:06:40 -0800

    zero state

--- extensions/wikia/NjordPrototype/NjordCharacterController.class.php
+++ extensions/wikia/NjordPrototype/NjordCharacterController.class.php
@@ -5,5 +5,15 @@ class NjordCharacterController extends WikiaController {
 	public function index() {
 		$params = $this->request->getParams();
 		$this->characterModel = !empty( $params['characterModel'] ) ? $params['characterModel'] : null;
+		if ( !$this->wg->user->isLoggedIn() ) {
+			//TODO: add check for zero state
+			return $this->skipRendering();
+		}
+		$this->wg->out->addStyle( AssetsManager::getInstance()->getSassCommonURL( 'extensions/wikia/NjordPrototype/css/NjCharacter.scss' ) );
+
+		$this->isAllowedToEdit = $this->wg->user->isAllowed( 'njordeditmode' );
+		$wd = new stdClass();
+		$wd->title = 'Characters';
+		$this->wikiData = $wd;
 	}
 }
--- extensions/wikia/NjordPrototype/templates/NjordCharacter_index.php
+++ extensions/wikia/NjordPrototype/templates/NjordCharacter_index.php
@@ -3,6 +3,43 @@
  * @var $characterModel CharacterModuleModel
  */
 ?>
+<div class="mom-character-module no-edit-state">
+	<div class="bar">
+		<div
+			class="title-wrap sg-sub-title <?php if ( isset( $wikiData->title ) ) : ?>filled-state<? else : ?>zero-state<?php endif; ?>">
+			<div class="edit-box">
+				<div class="mc-title" contenteditable="true"><?= $wikiData->title ?></div>
+				<div class="btn-bar">
+					<div class="new-btn inverse-btn discard-btn sg-sub">Discard</div>
+					<div class="new-btn default-btn save-btn sg-sub">Publish</div>
+				</div>
+			</div>
+			<span class="title-text"><?= $wikiData->title ?></span>
+			<span class="title-default-text">Characters</span>
+			<? if ( $isAllowedToEdit ): ?>
+				<img class="title-edit-btn" src="/extensions/wikia/NjordPrototype/images/pencil_b.svg">
+			<? endif; ?>
+		</div>
+		<div class="btn-group">
+			<div class="new-btn default-btn add-btn sg-sub">Add article page</div>
+			<div class="new-btn inverse-btn settings-btn sg-sub"></div>
+		</div>
+	</div>
+	<ul class="items-list <?php if ( isset( $wikiData->elements ) ) : ?>filled-state<? else : ?>zero-state<?php endif; ?>">
+		<div class="add-block">
+			<div class="main-add-text">
+				<span class="sg-main">Add an Article</span>
+			</div>
+			<div class="add-text sg-sub">Click on
+				<span class="add-sub-text">+add article page</span>
+				button on the top right corner
+			</div>
+		</div>
+		<li class="item"></li>
+		<li class="item"></li>
+		<li class="item"></li>
+		<li class="item"></li>
+	</ul>
 <div class="mom-character-module">
 	<?php foreach ( $characterModel->contentSlots as $contentSlot ): ?>
 	<section class="mom-character">
