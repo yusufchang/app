31edd6279c62579d82144bf86b76aeb70dcc868c
sebastian-wikia <sebastianm@wikia-inc.com>
2014-11-11 13:40:13 -0800

    DAT-2182 WIP; Serve the data and thumbnail for Character Module

--- extensions/wikia/NjordPrototype/models/CharacterModuleModel.class.php
+++ extensions/wikia/NjordPrototype/models/CharacterModuleModel.class.php
@@ -1,6 +1,10 @@
 <?php
 
 class CharacterModuleModel {
+	const WIKI_CHARACTER_IMAGE_MAX_WIDTH = 350;
+	const WIKI_CHARACTER_IMAGE_MAX_HEIGHT = 350;
+	const THUMBNAILER_SIZE_SUFIX = '350px-0';
+
 	public $contentSlots = [ ];
 
 	public function __construct( $pageName ) {
@@ -13,13 +17,76 @@ class CharacterModuleModel {
 		foreach ( $elements as $element ) {
 			$attributes = preg_split( '/\|/m', $element );
 			$item = new ContentEntity();
-			$item->link = $attributes[0];
-			$item->image = $attributes[1];
-			$item->title = $attributes[2];
-			$item->description = $attributes[3];
+
+			$item->link = $this->getLink( $attributes );
+			$item->image = $this->getImage( $attributes );
+			$item->title = $this->getTitle( $attributes );
+			$item->description = $this->getDescription( $attributes );
 
 			$items[] = $item;
 		}
 		$this->contentSlots = $items;
+	}
+
+	/**
+	 * @param $attributes
+	 * @param $item
+	 */
+	protected function getLink( $attributes ) {
+		$link = null;
+
+		if ( !empty( $attributes[0] ) ) {
+			$linkTitle = Title::newFromText( $attributes[0] );
+			if ( $linkTitle instanceof Title ) {
+				$link = $linkTitle->getLocalURL();
+			}
+		}
+
+		return $link;
+	}
+
+	/**
+	 * @param $attributes
+	 * @param $item
+	 */
+	protected function getImage( $attributes ) {
+		$image = null;
+
+		$imageTitle = Title::newFromText( $attributes[1], NS_FILE );
+		if ( $imageTitle instanceof Title ) {
+			$imageFile = wfFindFile( $imageTitle );
+			if ( $imageFile instanceof File && $imageFile->exists() ) {
+				$image = wfReplaceImageServer( $imageFile->getThumbUrl( self::THUMBNAILER_SIZE_SUFIX ) );
+			}
+		}
+		return $image;
+	}
+
+	/**
+	 * @param $attributes
+	 * @param $item
+	 */
+	protected function getTitle( $attributes ) {
+		$title = null;
+
+		if ( !empty( $attributes[2] ) ) {
+			$title = $attributes[2];
+		}
+
+		return $title;
+	}
+
+	/**
+	 * @param $attributes
+	 * @param $item
+	 */
+	protected function getDescription( $attributes ) {
+		$description = null;
+
+		if ( !empty( $attributes[3] ) ) {
+			$description = $attributes[3];
+		}
+
+		return $description;
 	}
 }
