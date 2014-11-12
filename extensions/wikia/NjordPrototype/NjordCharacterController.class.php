d622df65a07b56d30657e7e8342ba2a812f89088
Adam Robak <adamr@wikia-inc.com>
2014-11-11 17:31:28 -0800

    mock removed

--- extensions/wikia/NjordPrototype/NjordCharacterController.class.php
+++ extensions/wikia/NjordPrototype/NjordCharacterController.class.php
@@ -6,10 +6,6 @@ class NjordCharacterController extends WikiaController {
 		$characterModel = new CharacterModuleModel( Title::newMainPage()->getText() );
 		$characterModel->getFromProps();
 		$this->characterModel = $characterModel;
-		$this->characterModel = new CharacterModuleModel( Title::newMainPage()->getText() );
-		$this->characterModel->setFromContent('Category:TV series|Image:Mainpageportal1.png|Series and movies|Description 2
-Category:Media|Image:Mainpageportal2.png|Media|Description 3
-Category:In-universe articles|Image:Mainpageportal3.png|Around the universe|Description 4');
 		if ( !$this->wg->user->isLoggedIn() ) {
 			//TODO: add check for zero state
 			return $this->skipRendering();
