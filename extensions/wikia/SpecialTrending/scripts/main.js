3bb82931ed682bd5b0e9e85df3b9225451db16c3
V <bartosz.bentkowski@gmail.com>
2014-12-19 13:46:43 +0100

    Styling v0

--- extensions/wikia/SpecialTrending/scripts/main.js
+++ extensions/wikia/SpecialTrending/scripts/main.js
@@ -3,13 +3,24 @@ require(['jquery'], function($) {
 
 	/* global c3 */
 
-	c3.generate({
-		data: {
-			columns: [
-				['data1', 30, 200, 100, 400, 150, 250],
-				['data2', 50, 20, 10, 40, 15, 25],
-				['data3', 130, 150, 200, 300, 200, 100]
-			]
-		}
-	});
+	//c3.generate({
+	//	data: {
+	//		columns: [
+	//			['data1', 30, 200, 100, 400, 150, 250],
+	//			['data2', 50, 20, 10, 40, 15, 25],
+	//			['data3', 130, 150, 200, 300, 200, 100]
+	//		]
+	//	}
+	//});
+
+	var $tabs = $('.tab'),
+		$buttons = $('.tabs a');
+
+	$buttons.on('click', function(e) {
+		e.preventDefault();
+		var name = $(this).data('tab');
+
+		$buttons.removeClass('active').find('[data-tab='+name+']').addClass('active');
+		$tabs.removeClass('active').find('[data-tab='+name+']').addClass('active');
+	}).first().click();
 });
--- extensions/wikia/SpecialTrending/templates/SpecialTrendingController_index.php
+++ extensions/wikia/SpecialTrending/templates/SpecialTrendingController_index.php
@@ -1,41 +1,48 @@
-<h2>Trending</h2>
-<table style="color: green">
-	<thead>
+<ul class="tabs">
+	<li><a href="#" data-tab="popular">Trending</a></li>
+	<li><a href="#" data-tab="unpopular">Biggest Loosers</a></li>
+</ul>
+<div class="tab" data-tab="popular">
+	<h2>Trending</h2>
+	<table style="color: green">
+		<thead>
+			<tr>
+				<td>Rank</td>
+				<td>Title</td>
+				<td>Pageviews change</td>
+			</tr>
+		</thead>
+		<tbody>
+			<? for($i=0; $i < count($trendingArticles); $i++): ?>
+			<tr>
+				<td><?=$i+1?>.</td>
+				<td><a href="<?=$trendingArticles[$i]['url'] ?>"><?=$trendingArticles[$i]['title'] ?></a></td>
+				<td><?=$trendingArticles[$i]['pvDiff'] ?></td>
+			</tr>
+			<? endfor ?>
+		</tbody>
+	</table>
+</div>
+
+<div class="tab" data-tab="popular">
+	<h2>Biggest Loosers</h2>
+	<table>
+		<thead>
 		<tr>
 			<td>Rank</td>
 			<td>Title</td>
 			<td>Pageviews change</td>
 		</tr>
-	</thead>
-	<tbody>
-		<? for($i=0; $i < count($trendingArticles); $i++): ?>
-		<tr>
-			<td><?=$i+1?>.</td>
-			<td><a href="<?=$trendingArticles[$i]['url'] ?>"><?=$trendingArticles[$i]['title'] ?></a></td>
-			<td><?=$trendingArticles[$i]['pvDiff'] ?></td>
-		</tr>
+		</thead>
+		<tbody>
+		<? for($i=0; $i < count($loosingArticles); $i++): ?>
+			<tr>
+				<td><?=$i+1?>.</td>
+				<td><a href="<?=$loosingArticles[$i]['url'] ?>"><?=$loosingArticles[$i]['title'] ?></a></td>
+				<td><?=$loosingArticles[$i]['pvDiff'] ?></td>
+			</tr>
 		<? endfor ?>
-	</tbody>
-</table>
-
-
-<h2>Biggest Loosers</h2>
-<table>
-	<thead>
-	<tr>
-		<td>Rank</td>
-		<td>Title</td>
-		<td>Pageviews change</td>
-	</tr>
-	</thead>
-	<tbody>
-	<? for($i=0; $i < count($loosingArticles); $i++): ?>
-		<tr>
-			<td><?=$i+1?>.</td>
-			<td><a href="<?=$loosingArticles[$i]['url'] ?>"><?=$loosingArticles[$i]['title'] ?></a></td>
-			<td><?=$loosingArticles[$i]['pvDiff'] ?></td>
-		</tr>
-	<? endfor ?>
-	</tbody>
-</table>
+		</tbody>
+	</table>
+</div>
 <div id="chart"></div>
