1bc7b2b905a8e840450064b40dfbdfa5dd22046a
Garth Webb <garth@wikia-inc.com>
2015-02-23 16:29:17 -0800

    Added comments

--- extensions/wikia/SomeExtension/tests/SomeExtensionTest.php
+++ extensions/wikia/SomeExtension/tests/SomeExtensionTest.php
@@ -4,6 +4,7 @@
  * Class TestRef
  */
 class SomeClassDef {
+	// Obligatory public function to call private functions
 	public function change( $arr ) {
 		print_r($arr);
 		$this->changeParam( $arr );
@@ -11,11 +12,13 @@ class SomeClassDef {
 		print_r($arr);
 	}
 
+	// Changes to $array will not leave this function
 	private function noChangeParam( $array ) {
 		unset( $array['foo'] );
 		return 'notChanged';
 	}
 
+	// Due to pass by reference, changes to $array will leave this function
 	private function changeParam( &$array ) {
 		unset( $array['foo'] );
 		return 'changed';
@@ -29,11 +32,13 @@ class TestTest extends WikiaBaseTest {
 	public function testChangeParam() {
 		$arr = [ 'foo' => 'bar' ];
 
+		// Test that we can successfully mock a private method
 		$noChangeMethod = new ReflectionMethod( 'SomeClassDef', 'noChangeParam' );
 		$noChangeMethod->setAccessible( true );
 		$result = $noChangeMethod->invoke( new SomeClassDef(), $arr );
 		$this->assertTrue( $result == 'notChanged', 'Calling noChangeParam' );
 
+		// Test that will fail on the 'invoke' line.
 		$changeMethod = new ReflectionMethod( 'SomeClassDef', 'changeParam' );
 		$changeMethod->setAccessible( true );
 		$result = $changeMethod->invoke( new SomeClassDef(), $arr );
