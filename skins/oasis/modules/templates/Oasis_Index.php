9a46058ae0ebad8713771593a67352f071e4350e
Damian Jozwiak <damian@wikia-inc.com>
2015-06-08 13:03:56 +0200

    SOC-842 | Prototype login modal - join screen

--- skins/oasis/modules/templates/Oasis_Index.php
+++ skins/oasis/modules/templates/Oasis_Index.php
@@ -101,7 +101,7 @@
 <?= $bottomScripts ?>
 
 
-<iframe src="/login?redirect=<?php echo urlencode('//' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'])?>" style="position:absolute; top: 0; right:0; height: 600px; z-index: 10;" onload="this.style.height = this.contentWindow.document.body.scrollHeight + 'px';"></iframe>
+<iframe src="/join?redirect=<?php echo urlencode('//' . (isset($_SERVER['HTTP_X_ORIGINAL_HOST']) ? $_SERVER['HTTP_X_ORIGINAL_HOST'] : $_SERVER['HTTP_HOST']) . $_SERVER['REQUEST_URI'])?>" style="position:absolute; top: 0; right:0; height: 600px; z-index: 10;" onload="this.style.height = this.contentWindow.document.body.scrollHeight + 'px';"></iframe>
 </body>
 
 <?= wfReportTime() . "\n" ?>
