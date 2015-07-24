2b755381e683f75483d566e47c19b9d800ffd314
nAndy <nandy@wikia-inc.com>
2015-07-24 15:10:30 +0000

    moved flite poll to different tpl and paramatrized it

--- extensions/wikia/PortableInfobox/services/PortableInfoboxRenderService.class.php
+++ extensions/wikia/PortableInfobox/services/PortableInfoboxRenderService.class.php
@@ -119,10 +119,6 @@ class PortableInfoboxRenderService extends WikiaService {
 			if ( $this->isWikiaMobile() ) {
 				$type = $type . self::MOBILE_TEMPLATE_POSTFIX;
 			}
-//
-//		return $this->templateEngine->clearData()
-//			->setData( $data )
-//			->render( $this->templates[ 'poll' ] );
 		}
 
 		if ( $this->isWikiaMobile() ) {
