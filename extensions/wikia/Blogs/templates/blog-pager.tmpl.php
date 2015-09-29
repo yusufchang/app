6548133b4a5fa4f5fcc8fa91c822fbd2f37ae4f6
Piotr Gabryjeluk <rychu@wikia-inc.com>
2015-09-29 14:08:09 -0700

    RYCHU DEBUG

--- extensions/wikia/Blogs/templates/blog-pager.tmpl.php
+++ extensions/wikia/Blogs/templates/blog-pager.tmpl.php
@@ -5,11 +5,11 @@
 $i = 0;
 if ($iPageCount > 1) {
 	if ( $iPage != 0 ) {
-?>		
+?>
 <a href="<?=$wgTitle->getFullURL($pageOffsetName."=".($iPage - 1))?>">&#171; <?=wfMsg('blog-newerposts')?></a>
 <?
 	}
-?>	
+?>
 &nbsp;&nbsp;<b><?=($iPage+1)?></b>&nbsp;&nbsp;
 <?
 	if ( ($iPage + 1) != $iPageCount ) {
@@ -17,7 +17,9 @@ if ($iPageCount > 1) {
 <a href="<?=$wgTitle->getFullURL($pageOffsetName."=".($iPage+1))?>"><?=wfMsg('blog-olderposts')?> &#187;</a>
 <?
 	}
-} 
+}
 ?>
 </td><td width="15%">&nbsp;</td></tr></table>
 <!-- e:<?= __FILE__ ?> -->
+
+<pre><? debug_print_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS); ?></pre>
