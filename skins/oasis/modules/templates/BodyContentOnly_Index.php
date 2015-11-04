f3f333eda5fc7c3174a31760284d489cfc717308
nAndy <nandy@wikia-inc.com>
2015-11-03 17:59:48 -0800

    ADEN-2591 added div wrapper and try-catch GCS code

--- skins/oasis/modules/templates/BodyContentOnly_Index.php
+++ skins/oasis/modules/templates/BodyContentOnly_Index.php
@@ -1,8 +1,15 @@
 <section id="WikiaPage" class="WikiaPage<?= empty( $wg->OasisNavV2 ) ? '' : ' V2' ?>">
 	<div id="WikiaPageBackground" class="WikiaPageBackground"></div>
 	<div class="WikiaPageContentWrapper">
-		<div id="WikiaArticle" class="WikiaArticle">
-			<?= $bodytext ?>
+		<!-- ADEN-2591 not meant for production START -->
+		<div class="p402_premium">
+			<div id="WikiaArticle" class="WikiaArticle">
+				<?= $bodytext ?>
+			</div>
 		</div>
+		<script type="text/javascript">
+			try { _402_Show(); } catch(e) {}
+		</script>
+		<!-- ADEN-2591 not meant for production END -->
 	</div>
 </section>
