d8c0e585d725bba3c0811d649fbf4ab5d5fee1cb
Inez Korczynski <inez@wikia-inc.com>
2015-11-20 02:37:01 +0000

    Getting rid of mw.loader.use - because now mw.loader.using provides same functionality

--- resources/wikia/wikia.mediawiki.js
+++ resources/wikia/wikia.mediawiki.js
@@ -1,20 +1,8 @@
 (function( mw, $) {
-
 	if ( mw.loader && typeof mw.loader.use == 'undefined' ) {
-	    mw.loader.use = function( modules ) {
-	        var deferred = $.Deferred();
-	        try {
-	            mw.loader.using(modules, function() {
-	                deferred.resolve();
-	            }, function() {
-	                deferred.reject();
-	            });
-	        } catch (e) {
-	            mw.log(e.toString());
-	            deferred.reject();
-	        }
-	        return deferred.promise();
-	    }
+		mw.loader.use = function(/*arguments*/) {
+			// TODO: Log this call as depracated
+			return mw.loader.using.apply(mw.loader, arguments);
+		}
 	}
-
 })(window.mediaWiki, jQuery);
