9548a58f5b9ac4d8ddf8618cf95783a2aa4fd3b1
adamkarminski <adamkarminski@gmail.com>
2015-12-11 08:22:27 +0100

    ArticleSnippet Initial commit

---
+++ extensions/wikia/ArticleSnippet/ArticleSnippet.i18n.php
@@ -0,0 +1,10 @@
+<?php
+
+$messages = [];
+
+/**
+ * English
+ */
+$messages['en'] = [
+	'article-snippet-desc' => 'An extension that allows you to preview a snippet of an article next to a blue link to it. It uses a Portable Infobox as a data source.',
+];
---
+++ extensions/wikia/ArticleSnippet/ArticleSnippet.setup.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Wikia Article Snippet Extension
+ *
+ * @author Adam Karmiński <adamk@wikia-inc.com>
+ * @copyright (c) 2015 Wikia, Inc.
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
+ */
+
+$wgExtensionCredits['other'][] = [
+	'name'				=> 'Article Snippet',
+	'version'			=> '1.0',
+	'author'			=> 'Adam Karmiński',
+	'url'               => 'https://github.com/Wikia/app/tree/dev/extensions/wikia/ArticleSnippet',
+	'descriptionmsg'    => 'article-snippet-desc',
+];
