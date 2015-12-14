5207af3a141d1826d079462e71007fd864e0efb5
adamkarminski <adamkarminski@gmail.com>
2015-12-14 16:14:43 +0100

    ArticleSnippet Allow users to mark fields to highlight

--- extensions/wikia/ArticleSnippet/controllers/ArticleSnippetApiController.class.php
+++ extensions/wikia/ArticleSnippet/controllers/ArticleSnippetApiController.class.php
@@ -2,7 +2,7 @@
 
 class ArticleSnippetApiController extends WikiaApiController {
 
-	const HIGHLIGHTS_LIMIT = 5;
+	const DATA_FIELDS_LIMIT = 5;
 
 	private
 		$articleSnippet = [
@@ -52,23 +52,48 @@ class ArticleSnippetApiController extends WikiaApiController {
 			return $data;
 		}
 
+		$highlightedFields = $regularFields = [];
+
 		foreach ( $data[0]['data'] as $field ) {
 			if ( $field['type'] === 'title' ) {
 				$this->articleSnippet['title'] = $field['data']['value'];
+
 			} elseif ( $field['type'] === 'image' ) {
 				$this->articleSnippet['image'] = $field['data'][0]['url'];
-			} elseif ( $field['type'] === 'data' )
-			{
-				$this->articleSnippet['highlights'][] = array_map( [ new self(), 'stripTags' ], $field['data'] );
-			} elseif ( $field['type'] === 'group' )
-			{
+
+			} elseif ( $field['type'] === 'data' ) {
+				if ( isset( $field['data']['highlight'] ) ) {
+					$highlightedFields[] = array_map( [ new self(), 'stripTags' ], $field['data'] );
+				} else {
+					$regularFields[] = array_map( [ new self(), 'stripTags' ], $field['data'] );
+				}
+			} elseif ( $field['type'] === 'group' ) {
 				foreach ( $field['data']['value'] as $subfield ) {
 					if ( $subfield['type'] === 'data' ) {
-						$this->articleSnippet['highlights'][] = array_map( [ new self(), 'stripTags' ], $subfield['data'] );
+						if ( isset( $subfield['data']['highlight'] ) ) {
+							$highlightedFields[] = array_map( [ new self(), 'stripTags' ], $subfield['data'] );
+						} else {
+							$regularFields[] = array_map( [ new self(), 'stripTags' ], $subfield['data'] );
+						}
 					}
 				}
 			}
 		}
+
+		$highlightedFieldsCount = count( $highlightedFields );
+
+		if ( $highlightedFieldsCount > 1 ) {
+			usort( $highlightedFields, function ( $a, $b ) {
+				return (int)$a['highlight'] - (int)$b['highlight'];
+			} );
+		}
+
+		if ( $highlightedFieldsCount < self::DATA_FIELDS_LIMIT ) {
+			$diff = self::DATA_FIELDS_LIMIT - $highlightedFieldsCount;
+			$highlightedFields = array_merge( $highlightedFields, array_slice( $regularFields, 0, $diff ) );
+		}
+
+		$this->articleSnippet['highlights'] = $highlightedFields;
 
 		return $this->articleSnippet;
 	}
--- extensions/wikia/PortableInfobox/services/Parser/Nodes/NodeData.php
+++ extensions/wikia/PortableInfobox/services/Parser/Nodes/NodeData.php
@@ -3,11 +3,14 @@ namespace Wikia\PortableInfobox\Parser\Nodes;
 
 class NodeData extends Node {
 
+	const HIGHLIGHT_TAG_NAME = 'highlight';
+
 	public function getData() {
 		if ( !isset( $this->data ) ) {
 			$this->data = [
 				'label' => $this->getInnerValue( $this->xmlNode->{self::LABEL_TAG_NAME} ),
-				'value' => $this->getValueWithDefault( $this->xmlNode )
+				'value' => $this->getValueWithDefault( $this->xmlNode ),
+				'highlight' => $this->getXmlAttribute( $this->xmlNode, self::HIGHLIGHT_TAG_NAME ),
 			];
 		}
 
